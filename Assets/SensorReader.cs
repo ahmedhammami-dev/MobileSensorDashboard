90f4640c30ff2b3ff27cb79d93696d1e099bb37a
ahmedhammami-dev <hammamiahmed.dev@gmail.com>
2025-12-11 23:49:02 +0100

    adding files

---
+++ Assets/SensorReader.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SensorReader : MonoBehaviour
+{
+    public TMP_Text txtAccel, txtGyro, txtGPS, txtBattery, txtCam;
+    public RawImage cameraPreview;
+    WebCamTexture camTex; bool gyroEnabled; Gyroscope gyro;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Input.gyro.enabled = true;
+        gyro = Input.gyro;
+        gyroEnabled = SystemInfo.supportsGyroscope;
+        if (WebCamTexture.devices.Length > 0)
+        {
+            camTex = new WebCamTexture();
+            cameraPreview.texture = camTex;
+            camTex.Play();
+        }
+        StartCoroutine(StartLocationService());
+    }
+    IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            txtGPS.text = "GPS: désactivé";
+            yield break;
+        }
+        // Désir approx. : précision 1 m, maj toutes 0.5 m
+        Input.location.Start(1f, 0.5f);
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait-- > 0)
+            yield return new WaitForSeconds(1);
+        if (Input.location.status != LocationServiceStatus.Running)
+        {
+            txtGPS.text = "GPS: indisponible";
+            yield break;
+        }
+    }
+    void Update()
+    {
+        Vector3 a = Input.acceleration;
+        txtAccel.text = $"Acc: {a.x:F2}, {a.y:F2}, {a.z:F2}";
+        if (gyroEnabled)
+        {
+            var g = gyro.rotationRateUnbiased;
+            txtGyro.text = $"Gyro: {g.x:F2}, {g.y:F2}, {g.z:F2}";
+        }
+        else txtGyro.text = "Gyro: non supporté";
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            var l = Input.location.lastData;
+            txtGPS.text = $"GPS: {l.latitude:F6}, {l.longitude:F6} | v={l.horizontalAccuracy:F1}m";
+        }
+        float bl = SystemInfo.batteryLevel;
+        var bs = SystemInfo.batteryStatus;
+        txtBattery.text = $"Batt: {(bl < 0 ? "?" : (bl * 100f).ToString("F0") + "%")} ({bs})";
+        if (camTex != null)
+            txtCam.text = $"Caméra: {camTex.width}x{camTex.height}";
+    }
+    void OnDestroy()
+    {
+        if (camTex != null) camTex.Stop();
+        Input.location.Stop();
+    }
+}
